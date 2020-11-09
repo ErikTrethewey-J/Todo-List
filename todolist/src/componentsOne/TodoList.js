c2331bf9e4010fff2c69ab470ef91af549ce3cb9
ErikTrethewey-J <erikenobi@gmail.com>
2020-11-09 15:27:36 -0800

    Finally understanding how props work!

--- todolist/src/componentsOne/TodoList.js
+++ todolist/src/componentsOne/TodoList.js
@@ -1,9 +1,16 @@
 import React from 'react';
-
-const ToDoList = () => {
+// Import Components
+import Todo from './Todo'
+const ToDoList = ({ todos, setTodos }) => {
     return (
         <div className="todo-container">
-        <ul className="todo-list"></ul>
+            <ul className="todo-list">
+                {
+                    todos.map(todo => (
+                        <Todo setTodos={setTodos} todos={todos} key={ todo.id } text={todo.text} />
+                    ))
+                }
+            </ul>
       </div>
     )
 }
