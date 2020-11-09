9cabd7096bea03b1b40c51923764ee4831acac0b
ErikTrethewey-J <erikenobi@gmail.com>
2020-11-09 14:40:44 -0800

    Working on input components.

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css'
 // Importing Components
 import Form from './componentsOne/Form'
 import TodoList from './componentsOne/TodoList'
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState([])
+  
   return (
     <div className="App">
       <header>
         <h1>Erik's Todo List</h1>
       </header>
-      <Form />
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+      />
       <TodoList />
     </div>
   );
